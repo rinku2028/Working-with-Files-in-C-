c20cbe8fa656aac5aa27bb17e82976d31eb66c98
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-24 23:50:21 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace writetofile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Write text to the file!";  
+            File.WriteAllText("filename.txt", str);  // Creates a file name filename.txt and write the content of string str to the file created
+
+            string readText = File.ReadAllText("filename.txt");  // Read the contents of the file named filename.txt
+            Console.WriteLine("Content of file:"+"\t"+readText);  // Output of the content
+            Console.ReadLine();
+        }
+    }
+}
