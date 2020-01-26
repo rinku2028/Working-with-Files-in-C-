7d380ac01bf412bf95136e45525fb9397988d827
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-26 23:54:26 +0545

    Add files via upload

---
+++ fileuploadindrive.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="fileuploadindrive.aspx.cs" Inherits="fileupload.fileuploadindrive" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <asp:Button ID="btnupload" runat="server" Text="UPLOAD FILE WITH SIZE" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ fileuploadindrive.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace fileupload
+{
+    public partial class fileuploadindrive : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnupload_Click(object sender, EventArgs e)
+        {
+            string savePath = @"c:\temp\uploads\";
+            if (FileUpload1.HasFile)
+            {
+                int fileSize = upload1.PostedFile.ContentLength;
+                if (fileSize < 2100000)
+                {
+                    savePath += Server.HtmlEncode(upload1.FileName);
+                    upload1.SaveAs(savePath);
+                    lblMessage.Text = "File was uploaded successfully!!!";
+
+
+
+                }
+                else
+                {
+                    lblMessage.Text = "File was not uploaded because " + "file exceeds the 2 MB size limit.";
+
+
+                }
+                lblMessage.Text = "Please specify to upload file!!!";
+
+
+            }
+
+        }
+    }
+}
