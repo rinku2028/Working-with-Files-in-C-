598c0a30ee59bb8b13aeb03c0ebb396750bc3dd4
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-26 23:33:18 +0545

    Add files via upload

---
+++ fileupload.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="fileupload.aspx.cs" Inherits="fileupload.fileupload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="fileupload1" runat="server" />
+            <asp:Button ID="btnupload" runat="server" Text="Upload File" OnClick="btnupload_Click" />
+            <asp:Label ID="lblMessage" runat="server" Text="Label"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ fileupload.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace fileupload
+{
+    public partial class fileupload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnupload_Click(object sender, EventArgs e)
+        {
+            if (fileupload1.HasFile)
+            {
+                try
+                {
+
+                    fileupload1.SaveAs("E:\\" + fileupload1.FileName);
+
+                    lblMessage.Text = "File uploaded succesfully in C:Drive";
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = "File not uploaded!!" + ex.Message.ToString();
+
+                }
+
+            }
+            else
+            {
+                lblMessage.Text = "Please select the file and upload again!!!";
+            }
+        }
+    }
+}
