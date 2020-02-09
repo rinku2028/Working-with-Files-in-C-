412ebe85b69761450cb5fafdcf56dd6f28eb908b
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-10 01:11:52 +0545

    Add files via upload

---
+++ openbrowserfromconsole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace allfiles
+{
+    class openbrowserfromconsole
+    {
+        static void Main(string[] args)
+        {
+            Process myProcess = new Process();
+
+            try
+            {
+                // true is the default, but it is important not to set it to false
+                myProcess.StartInfo.UseShellExecute = true;
+                myProcess.StartInfo.FileName = "http://google.com";
+                myProcess.Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
