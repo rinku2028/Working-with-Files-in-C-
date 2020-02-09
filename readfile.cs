8b9d5dbefadc691f95339a1652e68d1f76f55fe7
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-10 00:38:20 +0545

    Read from file

---
+++ readfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace deleteallfiles
+{
+    class readfile
+    {
+        static void Main(string []args)
+        {
+            string text;
+            using (var streamReader = new StreamReader(@"e:\file.txt", Encoding.UTF8))
+            {
+                text = streamReader.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            Console.ReadLine();
+        }
+    }
+}
