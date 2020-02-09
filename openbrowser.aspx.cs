3f6de7f89740c3849a8940bb4e39248249cd4430
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-10 00:48:19 +0545

    Add files via upload

---
+++ openbrowser.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class openbrowser : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Open default browser 
+            System.Diagnostics.Process.Start("http://google.com");
+
+        }
+    }
+}
