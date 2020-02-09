28b76306997704301437e851eb29bfdad60f3d52
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-10 01:18:43 +0545

    Add files via upload

---
+++ getdirectory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace allfiles
+{
+    class getdirectory
+    {
+        static void Main(string []args)
+        {
+            string path = Path.GetDirectoryName(Assembly.GetAssembly(typeof(getdirectory)).CodeBase);
+            Console.WriteLine(path);//Prints the location of Directory of the class file
+            Console.ReadLine();
+
+        }
+    }
+}
