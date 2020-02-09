51d52669c1f1791ce7e5630130a22fe9788782a9
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-10 00:51:05 +0545

    Open image

---
+++ openimage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace allfiles
+{
+    class openimage
+    {
+        static void Main(string []args)
+        {
+            //opens default given image from e drive
+            System.Diagnostics.Process.Start(@"e:\image.jpg");
+
+        }
+    }
+}
