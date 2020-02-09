82c36643a9679cf6f6730306946cb059053e1674
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-10 00:10:58 +0545

    Delete files from the folder
    Delete all files from the given location

---
+++ deleteallfiles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace deleteallfiles
+{
+    class deleteallfiles
+    {
+       //How to delete all files(*.*) from a folder in C#.
+
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] filePaths = Directory.GetFiles(@"c:\mydir\");
+                foreach (string filePath in filePaths)
+                    File.Delete(filePath);
+                Console.WriteLine("The files has been deleted in c drive of mydir folder.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Location of the folder cannot be found!");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
